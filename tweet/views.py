42d5c4f4cb8ca7c177632e48ac4f92341909d3be
mavrick w <mavwatts@gmail.com>
2020-09-08 20:40:00 -0400

    commiting function to class based views change

--- tweet/views.py
+++ tweet/views.py
@@ -4,13 +4,39 @@ from tweet.forms import TweetForm
 from tweet.models import Tweet
 from twitteruser.models import TwitterUser
 from notification.models import Notification
+from django.views.generic.base import View
 
 import re
 
 # Create your views here.
-@login_required
-def add_tweet_view(request):
-    if request.method == 'POST':
+# @login_required
+# def add_tweet_view(request):
+#     if request.method == 'POST':
+#         form = TweetForm(request.POST)
+#         if form.is_valid():
+#             data = form.cleaned_data
+#             tweet_post = Tweet.objects.create(
+#                 tweet=data['tweet'],
+#                 tweet_maker=request.user
+#             )
+#             if "@" in data['tweet']:
+#                 recipients = re.findall(r'@(\w+)', data.get("tweet"))
+#                 for recipient in recipients:
+#                     match_user = TwitterUser.objects.get(username=recipient)
+#                     if match_user:
+#                         message = Notification.objects.create(msg_content=tweet_post, receiver=match_user)
+#             return HttpResponseRedirect(reverse('homepage'))
+
+#     form = TweetForm()
+#     return render(request, 'generic_form.html', {'form': form})
+
+
+class AddTweetView(View):
+    def get(self, request):
+        form = TweetForm()
+        return render(request, 'generic_form.html', {'form': form})
+
+    def post(self, request):
         form = TweetForm(request.POST)
         if form.is_valid():
             data = form.cleaned_data
@@ -26,12 +52,15 @@ def add_tweet_view(request):
                         message = Notification.objects.create(msg_content=tweet_post, receiver=match_user)
             return HttpResponseRedirect(reverse('homepage'))
 
-    form = TweetForm()
-    return render(request, 'generic_form.html', {'form': form})
 
-def tweet_detail_view(request, tweet_id):
-    comment = Tweet.objects.filter(id=tweet_id).first()
-    return render(request, 'tweet_detail.html', {"comment":comment} )
 
+# def tweet_detail_view(request, tweet_id):
+#     comment = Tweet.objects.filter(id=tweet_id).first()
+#     return render(request, 'tweet_detail.html', {"comment":comment} )
+
+class TweetDetailView(View):
+    def get(self, request, tweet_id):
+        comment = Tweet.objects.filter(id=tweet_id).first()
+        return render(request, 'tweet_detail.html', {"comment":comment} )
 
 
